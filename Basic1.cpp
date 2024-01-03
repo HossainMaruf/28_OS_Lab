3902108f8a740d6eb7528e470375e682b4e0aa72
HossainMaruf <maruficepustian@gmail.com>
2024-01-03 10:34:29 +0600

    Initial

---
+++ Basic1.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int id;
+  string name, fName, mName;
+  cout << "Enter id: ";
+  cin>>id;
+  cout << "Enter name: ";
+  cin>>name;
+  cout << "Enter father's name: ";
+  cin>>fName;
+  cout << "Enter mother's name: ";
+  cin>>mName;
+  // Display Information
+  cout << "ID: " << id << endl;
+  cout << "Name: " << name << endl;
+  cout << "Father's Name: " << fName << endl;
+  cout << "Mother's Name: " << mName << endl;
+
+  return 0;
+}
