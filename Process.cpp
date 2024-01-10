b8222cdb4e18fe6034a377454a18067a251f7b39
HossainMaruf <maruficepustian@gmail.com>
2024-01-10 10:24:56 +0600

    using stdin file

---
+++ Process.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Process {
+  int at; // Arrival Time
+  int priority;
+};
+
+typedef struct Process Process;
+
+int main() {
+  #ifndef MARUF
+    freopen("input.txt", "r", stdin);
+  #endif
+
+  int num1, num2, num3;
+  cin>>num1>>num2>>num3;
+  cout << num1 <<" " << num2 << " " << num3;
+
+
+
+  return 0;
+}
