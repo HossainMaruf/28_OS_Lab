f315d0feb0611c854f16a470923352c354e9eea5
HossainMaruf <maruficepustian@gmail.com>
2024-01-10 10:46:20 +0600

    sorting using multiple criteria

--- Process.cpp
+++ Process.cpp
@@ -7,6 +7,34 @@ struct Process {
   int priority;
 };
 
+void display(Process p[], int n) {
+  cout << endl;
+  for(int i=0; i<n; i++) {
+    cout << "ID: " << i+1 << "\t" << p[i].at << "\t" << p[i].priority << endl;
+  }
+  cout << endl << endl;
+}
+
+bool compAt(Process p1, Process p2) {
+  // if(p1.at < p2.at) return true;
+  // else return false;
+  return p1.at < p2.at;
+}
+
+bool compPriority(Process p1, Process p2) {
+  // if(p1.at < p2.at) return true;
+  // else return false;
+  return p1.priority < p2.priority;
+}
+
+bool compAtPriority(Process p1, Process p2) {
+  if(p1.at == p2.at) {
+    return p1.priority < p2.priority;
+  } else {
+    return p1.at < p2.at;
+  }
+}
+
 typedef struct Process Process;
 
 int main() {
@@ -14,10 +42,26 @@ int main() {
     freopen("input.txt", "r", stdin);
   #endif
 
-  int num1, num2, num3;
-  cin>>num1>>num2>>num3;
-  cout << num1 <<" " << num2 << " " << num3;
+  int n;
+  cout << "Enter number of process: ";
+  cin >> n;
+  Process p[n];
 
+  for(int i=0; i<n; i++) {
+    cout << "Enter at and priority of process: #" << i+1 << ": ";
+    cin >> p[i].at >> p[i].priority;
+  }
+
+  display(p, n);
+  // using at
+  sort(p, p+n, compAt);
+  display(p, n);
+  // using priority
+  sort(p, p+n, compPriority);
+  display(p, n);
+  // using multiple criteria 
+  sort(p, p+n, compAtPriority);
+  display(p, n);
 
 
   return 0;
